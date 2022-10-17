dbb416ddf63c7189eaa4ba309aed232fff4494df
Khaled Alghanim <al.ghanim@me.com>
2022-10-17 20:02:00 +0300

    CRUD is done!

--- bakery/urls.py
+++ bakery/urls.py
@@ -21,4 +21,8 @@ from stores import views as store_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("items/", store_views.get_store_items, name="store-item-list"),
+    path("items/create/", store_views.create_store_item, name="create-store-item"),
+    path("items/edit/<int:item_id>/", store_views.update_store_item, name="update-store-item"),
+    path("items/delete/<int:item_id>/", store_views.delete_store_item, name="delete-store-item"),
+    path("", store_views.index, name="index"),
 ]
--- stores/views.py
+++ stores/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
+from ast import Try
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render, redirect
 from stores import models
+from .forms import StoreItemForm
+from .models import StoreItem
 
 
 def get_store_items(request: HttpRequest) -> HttpResponse:
@@ -10,3 +12,54 @@ def get_store_items(request: HttpRequest) -> HttpResponse:
         "store_items": store_items,
     }
     return render(request, "store_item_list.html", context)
+
+
+def index(request):
+
+    store_items: list[models.StoreItem] = list(models.StoreItem.objects.all())
+    context = {
+        "store_items": store_items,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context)
+
+def create_store_item(request):
+    form = StoreItemForm()
+
+    if request.method == "POST":
+        form = StoreItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("store-item-list")
+
+
+    context = {
+        "form": form,
+    }
+
+
+    return render (request, "create_store_item.html", context)
+
+def update_store_item(request, item_id):
+    store_item = StoreItem.objects.get(id=item_id)
+    form = StoreItemForm(instance=store_item)
+    context = {
+        "store_item": store_item,
+        "form": form,
+    }
+    if request.method == "POST":
+        form = StoreItemForm(request.POST, instance=store_item)
+        if form.is_valid():
+            form.save()
+            return redirect("store-item-list")
+    return render(request, "update_store_item.html", context)
+
+def delete_store_item(request, item_id):
+    try:
+        store_item = StoreItem.objects.get(id=item_id)
+    except:
+        raise Http404
+    
+    store_item.delete()
+    return redirect("store-item-list")
